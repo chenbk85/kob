b555247e12f291b9bb6c41bedf6f0e85927fc51c
lijippy@163.com <lijippy@163.com>
2019-01-08 14:19:33 +0800

    ignore

--- build/third/googletest-release-1.8.1/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/liyinbin/github/gottingen/kob/third/googletest-release-1.8.1
-# Build directory: /Users/liyinbin/github/gottingen/kob/build/third/googletest-release-1.8.1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
