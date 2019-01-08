bb4de99cb1ec61dcbd111df525166c4f95028bbd
lijippy@163.com <lijippy@163.com>
2019-01-08 14:13:00 +0800

    remove build files

--- build/third/googletest-release-1.8.1/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/liyinbin/github/gottingen/kob/third/googletest-release-1.8.1/googletest
-# Build directory: /Users/liyinbin/github/gottingen/kob/build/third/googletest-release-1.8.1/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
